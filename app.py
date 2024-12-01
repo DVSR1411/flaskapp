5a4ef1c4bc27cee751bced8954b6f783d6fe63fc
DANDA VENKATA SATHWIK REDDY <165892101+DVSR1411@users.noreply.github.com>
2024-12-01 15:09:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'Hello, Sathwik!'
+    return 'Hello, This is a demo'
 if __name__ == '__main__':
     app.run(debug=True)
